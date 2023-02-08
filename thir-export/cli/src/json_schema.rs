2622c2ecbae3a521b4b214d77a495e3e5fd73077
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-02-08 15:17:49 +0100

    Error on stderr

--- thir-export/cli/src/json_schema.rs
+++ thir-export/cli/src/json_schema.rs
@@ -4,7 +4,7 @@ fn main() {
     match std::env::args().collect::<Vec<_>>().as_slice() {
         [_, destination] => export_schema_to(destination),
         [bin, _rest @ ..] => {
-            println!("Usage: {} OUTPUT_FILE", bin);
+            eprintln!("Usage: {} OUTPUT_FILE", bin);
             std::process::exit(1)
         }
         _ => panic!(),
