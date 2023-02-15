91a79191637502e3e29ee700d6fe222c3b9e025c
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-02-15 14:00:24 +0100

    Utils

--- thir-elab/lib/utils.ml
+++ thir-elab/lib/utils.ml
@@ -1,3 +1,18 @@
+open Base
+
 let todo () = failwith "unimplemented!"
 let ( << ) f g x = f (g x)
 let ( >> ) f g x = g (f x)
+
+let ( &&& ) (f: 'a -> 'b) (g: 'a -> 'c) (x: 'a): 'b * 'c
+  = f x, g x
+    
+let ( *** ) (f: 'a -> 'b) (g: 'c -> 'd) (l, r: 'a * 'c): 'b * 'd
+  = f l, g r
+
+let map_fst f = f *** Fn.id
+let map_snd g = Fn.id *** g
+
+let fst3 (x, _, _) = x
+let snd3 (_, y, _) = y
+let thd3 (_, _, z) = z
