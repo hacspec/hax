6dea18453551e4fa38031b05e78d2b5819553e21
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-03-03 08:41:42 +0100

    Fix relative path in CLI options

--- thir-export/cli/src/json_schema.rs
+++ thir-export/cli/src/json_schema.rs
@@ -1,8 +1,12 @@
-use thir_export::utils::export_schema_to;
+mod export_schema_to;
+use thir_export;
 
 fn main() {
     match std::env::args().collect::<Vec<_>>().as_slice() {
-        [_, destination] => export_schema_to(destination),
+        [_, destination] => {
+            let destination = thir_export::PathOrDash::from(std::ffi::OsStr::new(destination));
+            export_schema_to::export_schema_to(&destination)
+        }
         [bin, _rest @ ..] => {
             eprintln!("Usage: {} OUTPUT_FILE", bin);
             std::process::exit(1)
