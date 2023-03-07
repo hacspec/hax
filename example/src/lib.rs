fef6beb7b3e7d8506ada34fcdb80747f76fcd482
Franziskus Kiefer <franziskuskiefer@gmail.com>
2023-03-07 20:11:09 +0100

    started ast linter

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,43 +1,44 @@
 // #![feature(never_type)]
+#![allow(dead_code, unused)]
 
-// trait Features {
-//     type Mutability;
-//     type EarlyExit;
-// }
+trait Features {
+    type Mutability;
+    type EarlyExit;
+}
 
-// enum Expr<T: Features> {
-//     Loop {
-//         body: Box<Expr<T>>,
-//     },
-//     Break {
-//         body: Box<Expr<T>>,
-//         witness: T::EarlyExit,
-//     },
-//     Assign {
-//         var: String,
-//         value: Box<Expr<T>>,
-//         witness: T::Mutability,
-//     },
-// }
+enum Expr<T: Features> {
+    Loop {
+        body: Box<Expr<T>>,
+    },
+    Break {
+        body: Box<Expr<T>>,
+        witness: T::EarlyExit,
+    },
+    Assign {
+        var: String,
+        value: Box<Expr<T>>,
+        witness: T::Mutability,
+    },
+}
 
-// macro_rules! helper_macro {
-//     ($x:expr) => {
-//         $x + 10
-//     };
-// }
+macro_rules! helper_macro {
+    ($x:expr) => {
+        $x + 10
+    };
+}
 
-// macro_rules! library_foo {
-//     (make $x:ident 32 unsigned) => {
-//         $x as u32
-//     };
-// }
+macro_rules! library_foo {
+    (make $x:ident 32 unsigned) => {
+        $x as u32
+    };
+}
 
-// #[derive(Clone)]
-// struct Foo {
-//     field_a: u8,
-//     field_b: u16,
-//     field_c: u16,
-// }
+#[derive(Clone)]
+struct FooStruct {
+    field_a: u8,
+    field_b: u16,
+    field_c: u16,
+}
 
 // #[derive(Clone)]
 // enum Bar {
@@ -48,7 +49,7 @@
 //     },
 // }
 
-// fn f<T: Into<u32>>(tuple @ (a, b): (u8, u16), x: u8, t: T, foo: Foo, bar: Bar) -> u32 {
+// fn f<T: Into<u32>>(tuple @ (a, b): (u8, u16), x: u8, t: T, foo: FooStruct, bar: Bar) -> u32 {
 //     match bar {
 //         Bar::BarA(0, 0) => library_foo!(make x 32 unsigned),
 //         Bar::BarA(_, b) => (b as u32) + (foo.field_a as u32),
@@ -57,65 +58,89 @@
 //     }
 // }
 
+pub fn my_fun() {
+    let x = 5;
+    let _y = unsafe { 8 };
+}
+
+pub unsafe fn unsafe_fun() {
+    let _x = 7;
+}
+
+pub async fn async_fun() {
+    let _x = 8;
+}
+
+pub trait MyTrait {
+    fn run(&self);
+}
+
+pub fn trait_obj_fun(trait_obj: &dyn MyTrait) {
+    trait_obj.run();
+}
+
+pub fn trait_obj_fun2(trait_obj: &impl MyTrait) {
+    trait_obj.run();
+}
+
 mod hash;
 
-// #[derive(Copy, Clone)]
-// struct DummyStruct {
-//     dummy: u8
-// }
+#[derive(Copy, Clone)]
+struct DummyStruct {
+    dummy: u8,
+}
 
-// enum Hello {
-//     A(u8, u8),
-//     B { x: u8, y: u8 },
-// }
+enum Hello {
+    A(u8, u8),
+    B { x: u8, y: u8 },
+}
 
-// trait Foo: Copy + Clone {
-//     fn bar(self) -> u8 {
-//         123
-//     }
-// }
+trait Foo: Copy + Clone {
+    fn bar(self) -> u8 {
+        123
+    }
+}
 
-// impl Foo for DummyStruct {}
+impl Foo for DummyStruct {}
 
-// struct Hi {
-//     field1: u8,
-//     field2: u8,
-//     field3: u8,
-//     field4: u8,
-// }
+struct Hi {
+    field1: u8,
+    field2: u8,
+    field3: u8,
+    field4: u8,
+}
 
-// fn f<A: Foo>(x: u8, y: A) -> u8
-// {
-//     let foo = 1u16 + 4;
-//     let a = Hello::A(2, 5);
-//     let b = Hello::B { y: 99, x: 123 };
-//     let hi = Hi {
-//         field1: 10,
-//         field2: 20,
-//         field3: 30,
-//         field4: 40,
-//     };
-//     if true {
-//         3 + x
-//     } else {
-//         match 1 {
-//             0 => 12,
-//             1 => match b {
-//                 Hello::A(x, y) => 1,
-//                 Hello::B { y, x } => 2,
-//             },
-//             _ => match hi {
-//                 Hi {
-//                     field1,
-//                     field2,
-//                     field3,
-//                     field4,
-//                 } => field3,
-//             },
-//         }
-//     }
-// }
+fn f2<A: Foo>(x: u8, y: A) -> u8 {
+    let foo = 1u16 + 4;
+    let a = Hello::A(2, 5);
+    let b = Hello::B { y: 99, x: 123 };
+    let hi = Hi {
+        field1: 10,
+        field2: 20,
+        field3: 30,
+        field4: 40,
+    };
+    if true {
+        3 + x
+    } else {
+        match 1 {
+            0 => 12,
+            1 => match b {
+                Hello::A(x, y) => 1,
+                Hello::B { y, x } => 2,
+            },
+            _ => match hi {
+                Hi {
+                    field1,
+                    field2,
+                    field3,
+                    field4,
+                } => field3,
+            },
+        }
+    }
+}
 
-// fn g() -> u8 {
-//     f(3, DummyStruct{dummy: 9})
-// }
+fn g() -> u8 {
+    f2(3, DummyStruct { dummy: 9 })
+}
--- thir-export/cli/src/driver_thir_export.rs
+++ thir-export/cli/src/driver_thir_export.rs
@@ -17,15 +17,25 @@ extern crate rustc_hir;
 extern crate rustc_hir_analysis;
 extern crate rustc_index;
 extern crate rustc_interface;
+extern crate rustc_lint;
+extern crate rustc_lint_defs;
 extern crate rustc_middle;
 extern crate rustc_mir_build;
+extern crate rustc_session;
 extern crate rustc_span;
 extern crate rustc_target;
 extern crate rustc_type_ir;
 
+use rustc_ast::ast;
+use rustc_data_structures::sync::Lrc;
 use rustc_driver::{Callbacks, Compilation};
+use rustc_errors::DiagnosticId;
+use rustc_hir::hir_id::HirId;
 use rustc_interface::{interface::Compiler, Queries};
+use rustc_lint::{EarlyContext, EarlyLintPass, LateContext, LateLintPass, LintPass};
+use rustc_lint_defs::{declare_lint, declare_lint_pass};
 use rustc_middle::middle::region::Scope;
+use rustc_session::Session;
 
 use rustc_middle::ty::TyCtxt;
 use rustc_middle::{
@@ -42,7 +52,7 @@ use rustc_middle::{
 // mod options;
 // use options::Options;
 
-use thir_export;
+use thir_export::{self};
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -126,10 +136,166 @@ fn collect_macros(
     v.macro_calls
 }
 
+fn linter(crate_ast: &rustc_ast::ast::Crate, session: &Lrc<Session>, compiler: &Compiler) {
+    use {rustc_ast::ast::*, rustc_ast::visit::*, rustc_span::Span};
+
+    struct ExprLinter<'a> {
+        session: &'a Lrc<Session>,
+    }
+    impl<'ast, 'a> Visitor<'ast> for ExprLinter<'a> {
+        fn visit_expr(&mut self, ex: &'ast Expr) {
+            match &ex.kind {
+                ExprKind::Block(block, _) => match block.rules {
+                    rustc_ast::ast::BlockCheckMode::Unsafe(
+                        rustc_ast::ast::UnsafeSource::UserProvided,
+                    ) => {
+                        self.session.span_warn_with_code(
+                            block.span,
+                            "[Circus] Unsafe code is not supported",
+                            DiagnosticId::Lint {
+                                name: "Unsafe code".to_string(),
+                                has_future_breakage: true,
+                                is_force_warn: true,
+                            },
+                        );
+                    }
+                    _ => (),
+                },
+                _ => (), // eprintln!("found expression {:?} at {:?}", ex.kind, ex.span),
+            }
+        }
+    }
+    let mut linter = ExprLinter { session };
+    linter.visit_crate(crate_ast);
+
+    struct FnLinter<'a> {
+        session: &'a Lrc<Session>,
+    }
+    impl<'a> FnLinter<'a> {
+        fn no_trait_objects(&self, span: Span) {
+            self.session.span_warn_with_code(
+                span,
+                "[Circus] Trait objects are not supported",
+                DiagnosticId::Lint {
+                    name: "Trait objects".to_string(),
+                    has_future_breakage: true,
+                    is_force_warn: true,
+                },
+            );
+        }
+    }
+    impl<'ast, 'a> Visitor<'ast> for FnLinter<'a> {
+        fn visit_fn(&mut self, fk: FnKind<'ast>, _: Span, _: NodeId) {
+            match fk {
+                FnKind::Fn(_, ident, sig, ..) => {
+                    // eprintln!(" ... found function {:?} at {:?}", ident.name, ident.span);
+                    // Unsafe functions
+                    match sig.header.unsafety {
+                        ast::Unsafe::Yes(span) => {
+                            self.session.span_warn_with_code(
+                                span,
+                                "[Circus] Unsafe functions are not supported",
+                                DiagnosticId::Lint {
+                                    name: "Unsafe code".to_string(),
+                                    has_future_breakage: true,
+                                    is_force_warn: true,
+                                },
+                            );
+                        }
+                        _ => (),
+                    }
+                    // async functions
+                    match sig.header.asyncness {
+                        ast::Async::Yes { span, .. } => {
+                            self.session.span_warn_with_code(
+                                span,
+                                "[Circus] Async functions are not supported",
+                                DiagnosticId::Lint {
+                                    name: "Async code".to_string(),
+                                    has_future_breakage: true,
+                                    is_force_warn: true,
+                                },
+                            );
+                        }
+                        _ => (),
+                    }
+                    // XXX: ext?
+                    // parameters
+                    sig.decl.inputs.iter().for_each(|param| {
+                        // eprintln!(
+                        //     " >>> Checking {:?} with kind {:?}",
+                        //     param.span, param.ty.kind
+                        // );
+                        // No dyn/trait object
+                        match &param.ty.kind {
+                            ast::TyKind::TraitObject(_, _) => {
+                                self.no_trait_objects(param.ty.span);
+                            }
+                            ast::TyKind::Rptr(_, ty) => match ty.ty.kind {
+                                ast::TyKind::TraitObject(_, _) => {
+                                    self.no_trait_objects(param.ty.span);
+                                }
+                                ast::TyKind::ImplTrait(_, _) => {
+                                    self.no_trait_objects(param.ty.span);
+                                }
+                                _ => (),
+                            },
+                            ast::TyKind::ImplTrait(_, _) => {
+                                self.no_trait_objects(param.ty.span);
+                            }
+                            _ => (), // eprintln!("    param ty {:?}", param.ty.kind),
+                        }
+                    });
+                }
+                FnKind::Closure(..) => {
+                    // eprintln!("found a closure - unsupported?");
+                }
+            }
+        }
+    }
+    let mut linter = FnLinter { session };
+    linter.visit_crate(crate_ast);
+
+    struct ItemLinter {}
+    impl<'ast> Visitor<'ast> for ItemLinter {
+        fn visit_item(&mut self, i: &'ast Item) {
+            // eprintln!("found item {:?} at {:?}", i.ident.name, i.span);
+        }
+    }
+    let mut linter = ItemLinter {};
+    linter.visit_crate(crate_ast);
+
+    // struct Linter<'a> {
+    //     session: &'a Lrc<Session>,
+    // }
+    // impl<'a> LintPass for Linter<'a> {
+    //     fn name(&self) -> &'static str {
+    //         "Circus"
+    //     }
+    // }
+    // impl<'tcx, 'a> LateLintPass<'tcx> for Linter<'a> {
+    //     fn check_fn(
+    //         &mut self,
+    //         _: &LateContext<'tcx>,
+    //         _: rustc_hir::intravisit::FnKind<'tcx>,
+    //         _: &'tcx rustc_hir::FnDecl<'tcx>,
+    //         _: &'tcx rustc_hir::Body<'tcx>,
+    //         _: Span,
+    //         _: HirId,
+    //     ) {
+    //         eprintln!(" >>> late lint pass check fn");
+    //     }
+    // }
+    // let mut linter = Linter { session };
+    // compiler.register_lints();
+}
+
 struct DefaultCallbacks {
     options: thir_export::Options,
 }
+
 impl Callbacks for DefaultCallbacks {
+    // fn config(&mut self, config: &mut rustc_interface::Config) {}
     fn after_parsing<'tcx>(
         &mut self,
         _compiler: &Compiler,
@@ -139,10 +305,12 @@ impl Callbacks for DefaultCallbacks {
     }
     fn after_expansion<'tcx>(
         &mut self,
-        _compiler: &Compiler,
+        compiler: &Compiler,
         queries: &'tcx Queries<'tcx>,
     ) -> Compilation {
+        let session = compiler.session();
         let macro_calls = collect_macros(&queries.parse().unwrap().peek());
+        linter(&queries.parse().unwrap().peek(), session, compiler);
         queries
             .global_ctxt()
             .unwrap()
