7e16267751e2aa31325941ef6e44adbc298cc23e
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-04-17 09:44:08 +0200

    Add git-related informations for `--version` in the CLI

--- cli/build.rs
+++ cli/build.rs
@@ -1,7 +1,24 @@
-use std::error::Error;
-use vergen::EmitBuilder;
+macro_rules! set_empty_env_var_with_git {
+    ($var:literal, $args: expr) => {
+        if let None = option_env!($var) {
+            println!(
+                "cargo:rustc-env={}={}",
+                $var,
+                std::process::Command::new("git")
+                    .args($args)
+                    .output()
+                    .map(|output| String::from_utf8(output.stdout).unwrap())
+                    .unwrap_or("unknown".into())
+            );
+        }
+        println!("cargo:rurun-if-env-changed={}", $var);
+    };
+}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    EmitBuilder::builder().emit()?;
-    Ok(())
+fn main() {
+    set_empty_env_var_with_git!(
+        "CIRCUS_GIT_DESCRIBE",
+        ["describe", "--tags", "--always", "--abbrev=0"]
+    );
+    set_empty_env_var_with_git!("CIRCUS_GIT_COMMIT_HASH", ["rev-parse", "HEAD"]);
 }
