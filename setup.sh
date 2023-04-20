dd96145378af0ec5f4567b46a70ca9beb824fad8
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-04-20 09:07:20 +0200

    Installation: use `opam install`

--- setup.sh
+++ setup.sh
@@ -6,9 +6,4 @@ set -x -e
 cargo install --path cli/driver && cargo install --path cli/subcommands
 
 # Install the OCaml engine:
-cd engine
-opam install --deps-only .
-DYLD_LIBRARY_PATH=$(rustup run nightly-2022-12-06 rustc --print=sysroot)/lib dune build
-export PATH=$PATH:$PWD/_build/install/default/bin
-cd -
-
+opam install --yes --assume-depexts ./engine
