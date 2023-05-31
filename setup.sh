f4fd7aff0b744d4a385ccc28b480376d58ae2412
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-05-31 11:11:12 +0200

    Add `--force` flag to `cargo install`

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 set -x -e
 
 # Install the Rust CLI & frontend, providing `cargo-hax` and `driver-hax`:
-cargo install --path cli/driver && cargo install --path cli/subcommands
+for i in driver subcommands; do
+    cargo install --force --path "cli/$i";
+done
 
 # Install the OCaml engine:
 OPAMASSUMEDEPEXTS=1 opam install --yes ./engine
