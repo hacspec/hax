2b84a3532652f5eb4d6937e4ba0e8cefab154afd
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-06-06 16:10:19 +0200

    Document a bit `pretty_print_diagnostics.rs`

--- cli/subcommands/src/pretty_print_diagnostics.rs
+++ cli/subcommands/src/pretty_print_diagnostics.rs
@@ -1,3 +1,10 @@
+//! This binary is useful for the engine: from OCaml we need to pretty
+//! print Hax-errors so that we can inline those as failed items or
+//! failed expressions in the generated modules.
+
+//! Thus, this binary expects a JSON diagnostics error on its stdin
+//! and outputs on stdout its pretty, `Display`ed version.
+
 use hax_diagnostics::Diagnostics as D;
 use serde_json::{from_reader, Value};
 
