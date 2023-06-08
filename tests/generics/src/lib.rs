16afb92862bb938f7d1ad43b1ad3ca39b629b947
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-06-08 10:44:51 +0200

    Add example for const generics

---
+++ tests/generics/src/lib.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+
+fn dup<T: Clone>(x: T) -> (T, T) {
+    (x.clone(), x.clone())
+}
+
+fn foo<const LEN: usize>(arr: [usize; LEN]) -> usize {
+    let mut acc = LEN + 9;
+    for i in 0..LEN {
+        acc += arr[i];
+    }
+    acc
+}
