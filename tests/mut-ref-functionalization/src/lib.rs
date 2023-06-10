a7a5bd17cceff7dddbb078aa69ceae3c6401230f
Franziskus Kiefer <franziskuskiefer@gmail.com>
2023-06-10 15:08:41 +0200

    vec append test

--- tests/mut-ref-functionalization/src/lib.rs
+++ tests/mut-ref-functionalization/src/lib.rs
@@ -4,3 +4,16 @@ fn _f() {
     vec.push(2);
     vec.swap(0, 1);
 }
+
+fn build_vec() -> Vec<u8> {
+    vec![1, 2, 3]
+}
+
+fn _append() {
+    let mut vec1 = Vec::new();
+    let mut vec2 = vec![1u8, 2, 3];
+    // This works
+    vec1.append(&mut vec2);
+    // This doesn't work
+    vec1.append(&mut build_vec());
+}
