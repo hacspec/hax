07edc440f443fb3b6a3006d8c3d5b524a108c22c
Lasse Letager Hansen <lasse@letager.dk>
2023-06-13 15:39:05 +0200

    SSProve backend

---
+++ engine/lib/phases/phase_state_passing_loop.mli
@@ -0,0 +1,29 @@
+open! Prelude
+
+module Make
+    (F : Features.T
+           with type mutable_reference = Features.Off.mutable_reference
+            and type mutable_pointer = Features.Off.mutable_pointer
+            and type raw_pointer = Features.Off.raw_pointer
+            and type arbitrary_lhs = Features.Off.arbitrary_lhs
+            and type nontrivial_lhs = Features.Off.nontrivial_lhs
+            and type monadic_action = Features.Off.monadic_action
+            and type monadic_binding = Features.Off.monadic_binding
+            and type for_index_loop = Features.Off.for_index_loop) : sig
+  include module type of struct
+    module FA = F
+
+    module FB = struct
+      include F
+
+      (* include Features.Off.Mutable_variable *)
+      include Features.On.State_passing_loop
+    end
+
+    module A = Ast.Make (F)
+    module B = Ast.Make (FB)
+    module ImplemT = Phase_utils.MakePhaseImplemT (A) (B)
+  end
+
+  include ImplemT.T
+end
