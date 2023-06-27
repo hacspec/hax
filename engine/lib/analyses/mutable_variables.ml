78124a2d483baa41197cf331b6bcad54cb0933d8
Lasse Letager Hansen <lasse@letager.dk>
2023-06-27 17:11:20 +0200

    fmt

--- engine/lib/analyses/mutable_variables.ml
+++ engine/lib/analyses/mutable_variables.ml
@@ -25,13 +25,15 @@ module%inlined_contents Make (F : Features.T) = struct
           match x.v with
           | Fn { name; generics; body; params } ->
               let items, count = analyse_function_body body (snd y) in
-              (fst y @ [(name, items)], count)
+              (fst y @ [ (name, items) ], count)
           | _ -> y)
         ~init:([], 0) items
     in
-    let mut_map : (global_ident,
-                   ((local_ident * A.ty) * int) list,
-                   GlobalIdent.comparator_witness) Map.t =
+    let mut_map :
+        ( global_ident,
+          ((local_ident * A.ty) * int) list,
+          GlobalIdent.comparator_witness )
+        Map.t =
       List.fold_left
         ~f:(fun y x -> Map.set y ~key:(fst x) ~data:(snd x))
         ~init:(Map.empty (module GlobalIdent))
@@ -39,19 +41,18 @@ module%inlined_contents Make (F : Features.T) = struct
     in
     List.fold_left
       ~f:(fun y x ->
-          Map.set y ~key:x
-            ~data:
-              (let local_muts =
-                 match (Map.find mut_map x) with
-                 | Some l -> l
-                 | None -> []
-               in
-               (match Map.find (fst func_dep) x with
-                | Some l ->
-                  let l' = List.filter_map ~f:(Map.find y) l in
-                  let b = List.concat (List.map ~f:fst l') in
-                  List.map ~f:(fst >> fst) local_muts @ b
-                | None -> []), local_muts))
+        Map.set y ~key:x
+          ~data:
+            (let local_muts =
+               match Map.find mut_map x with Some l -> l | None -> []
+             in
+             ( (match Map.find (fst func_dep) x with
+               | Some l ->
+                   let l' = List.filter_map ~f:(Map.find y) l in
+                   let b = List.concat (List.map ~f:fst l') in
+                   List.map ~f:(fst >> fst) local_muts @ b
+               | None -> []),
+               local_muts )))
       ~init:(Map.empty (module GlobalIdent))
       (List.map ~f:fst mut_var_list)
 
@@ -64,12 +65,9 @@ module%inlined_contents Make (F : Features.T) = struct
            ~f:(fun x ->
              match x.e with
              | Assign { lhs = LhsLocalVar { var; typ }; witness } ->
-               Some (var, typ)
-             | Let
-                 {
-                   lhs = { p = PBinding { mut = Mutable _; var; typ } };
-                   _
-                 } ->
+                 Some (var, typ)
+             | Let { lhs = { p = PBinding { mut = Mutable _; var; typ } }; _ }
+               ->
                  Some (var, typ)
              | _ -> None)
            (Flatten.flatten_ast x))
