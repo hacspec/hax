4a4961ec7093d5c796c538750e1e74ccdeab61bc
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-06-27 11:06:34 +0200

    Revert "Remove Boxes"
    This reverts commit 5275516a572687a752a3a9f6b16f4d0fbc9f0caa.

--- frontend/exporter/src/all.rs
+++ frontend/exporter/src/all.rs
@@ -2350,6 +2350,10 @@ pub enum ExprKind {
     //     lint_level: LintLevel,
     //     value: Expr,
     // },
+    /// A `box <value>` expression.
+    Box {
+        value: Expr,
+    },
     /// TODO
     #[disable_mapping]
     MacroInvokation(MacroInvokation),
