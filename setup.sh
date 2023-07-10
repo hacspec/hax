55aac060e75eefdda3e8cd9402cf8f2b29e84f41
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-07-10 10:28:24 +0200

    `setup.sh`: refactor, fix #197 & adds more logs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -x -e
+set -eu
 
+# Ensures a given binary is available in PATH
 ensure_binary_available() {
     command -v "$1" >/dev/null 2>&1 || {
         printf '\e[31mError: binary \e[1m%s\e[0m\e[31m was not found.\e[0m\n' "$1"
@@ -10,14 +11,33 @@ ensure_binary_available() {
     }
 }
 
+# Installs the Rust CLI & frontend, providing `cargo-hax` and `driver-hax`
+install_rust_binaries() {
+    for i in driver subcommands; do
+        ( set -x; cargo install --force --path "cli/$i")
+    done
+}
+
+# Provides the `hax-engine` binary
+install_ocaml_engine() {
+    # Fixes out of memory issues (https://github.com/hacspec/hacspec-v2/issues/197)
+    {
+        # Limit the number of thread spawned by opam
+        export OPAMJOBS=2
+        # Make the garbadge collector of OCaml more agressive (see
+        # https://discuss.ocaml.org/t/how-to-limit-the-amount-of-memory-the-ocaml-compiler-is-allowed-to-use/797)
+        export OCAMLRUNPARAM="o=20"
+    }
+    # Make opam show logs when an error occurs
+    export OPAMERRLOGLEN=0
+    # Make opam ignore system dependencies (it doesn't handle properly certain situations)
+    export OPAMASSUMEDEPEXTS=1
+    (set -x; opam install --yes ./engine)
+}
+
 for binary in opam node rustup jq; do
     ensure_binary_available $binary
 done
+install_rust_binaries
+install_ocaml_engine
 
-# Install the Rust CLI & frontend, providing `cargo-hax` and `driver-hax`:
-for i in driver subcommands; do
-    cargo install --force --path "cli/$i";
-done
-
-# Install the OCaml engine:
-OPAMASSUMEDEPEXTS=1 opam install --yes ./engine
