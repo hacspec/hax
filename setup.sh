af7e88a885914860d2faa6b7b7489a309756c725
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-07-10 10:10:23 +0200

    `setup.sh`: ensure dependencies are installed

--- setup.sh
+++ setup.sh
@@ -2,6 +2,18 @@
 
 set -x -e
 
+ensure_binary_available() {
+    command -v "$1" >/dev/null 2>&1 || {
+        printf '\e[31mError: binary \e[1m%s\e[0m\e[31m was not found.\e[0m\n' "$1"
+        printf '\e[37m(Did you look at \e[1mManual installation\e[0m\e[37m in \e[1mREADME.md\e[0m\e[37m?)\e[0m.\n'
+        exit 1
+    }
+}
+
+for binary in opam node rustup jq; do
+    ensure_binary_available $binary
+done
+
 # Install the Rust CLI & frontend, providing `cargo-hax` and `driver-hax`:
 for i in driver subcommands; do
     cargo install --force --path "cli/$i";
