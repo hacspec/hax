2f41982a8d383b9ce1661ec03ea77bdde270ccfe
Lasse Letager Hansen <lasse@letager.dk>
2023-08-18 13:32:45 +0200

    ssprove update

--- cli/options/src/lib.rs
+++ cli/options/src/lib.rs
@@ -218,7 +218,7 @@ pub struct Options {
         value_delimiter = ',',
         default_values = [
             "hacspec_lib::array::array", "hacspec_lib::array::public_bytes", "hacspec_lib::array::bytes",
-            "hacspec_lib::math_integers::public_nat_mod", "hacspec_lib::math_integers::unsigned_public_integer",
+            "hacspec_lib::public_nat_mod", "hacspec_lib::math_integers::public_nat_mod", "hacspec_lib::math_integers::unsigned_public_integer",
         ],
     )]
     pub inline_macro_calls: Vec<Namespace>,
