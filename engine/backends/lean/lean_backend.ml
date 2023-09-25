884b9e872d55e77a59fa465e2f923d210dd9ace9
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2023-09-25 14:40:20 +0200

    Basis for Lean backend

--- engine/backends/lean/lean_backend.ml
+++ engine/backends/lean/lean_backend.ml
@@ -19,9 +19,101 @@ include
       let backend = Diagnostics.Backend.Lean
     end)
 
+module SubtypeToInputLanguage
+    (FA : Features.T
+            with type mutable_reference = Features.Off.mutable_reference
+             and type continue = Features.Off.continue
+             and type break = Features.Off.break
+             and type mutable_reference = Features.Off.mutable_reference
+             and type mutable_pointer = Features.Off.mutable_pointer
+             and type mutable_variable = Features.Off.mutable_variable
+             and type reference = Features.Off.reference
+             and type raw_pointer = Features.Off.raw_pointer
+             and type early_exit = Features.Off.early_exit
+             and type question_mark = Features.Off.question_mark
+             and type as_pattern = Features.Off.as_pattern
+             and type lifetime = Features.Off.lifetime
+             and type monadic_action = Features.Off.monadic_action
+             and type arbitrary_lhs = Features.Off.arbitrary_lhs
+             and type nontrivial_lhs = Features.Off.nontrivial_lhs
+             and type loop = Features.Off.loop
+             and type block = Features.Off.block
+             and type for_loop = Features.Off.for_loop
+             and type for_index_loop = Features.Off.for_index_loop
+             and type state_passing_loop = Features.Off.state_passing_loop) =
+struct
+  module FB = InputLanguage
+
+  include
+    Subtype.Make (FA) (FB)
+      (struct
+        module A = FA
+        module B = FB
+        include Features.SUBTYPE.Id
+        include Features.SUBTYPE.On.Monadic_binding
+        include Features.SUBTYPE.On.Construct_base
+        include Features.SUBTYPE.On.Slice
+        include Features.SUBTYPE.On.Macro
+      end)
+
+  let metadata = Phase_utils.Metadata.make (Reject (NotInBackendLang backend))
+end
+
 module BackendOptions = Backend.UnitBackendOptions
 module AST = Ast.Make (InputLanguage)
 
-let apply_phases (_opts: BackendOptions.t) (items: Ast.Rust.item list) = []
+open Phase_utils
 
-let translate (_opts: BackendOptions.t) (items: AST.item list) = []
+module TransformToInputLanguage =
+  [%functor_application
+  Phases.Reject.RawOrMutPointer(Features.Rust)
+  |> Phases.And_mut_defsite
+  |> Phases.Reconstruct_for_loops
+  |> Phases.Direct_and_mut
+  |> Phases.Reject.Arbitrary_lhs
+  |> Phases.Drop_blocks
+  |> Phases.Drop_references
+  |> Phases.Trivialize_assign_lhs
+  |> Phases.Reconstruct_question_marks
+  |> Side_effect_utils.Hoist
+  |> Phases.Local_mutation
+  |> Phases.Reject.Continue
+  |> Phases.Cf_into_monads
+  |> Phases.Reject.EarlyExit
+  |> Phases.Functionalize_loops
+  |> Phases.Reject.As_pattern
+  |> SubtypeToInputLanguage
+  |> Identity
+  ]
+  [@ocamlformat "disable"]
+
+let apply_phases (_opts: BackendOptions.t) (items: Ast.Rust.item list) = 
+  TransformToInputLanguage.ditems items
+
+module LeanNamePolicy = struct
+  include Concrete_ident.DefaultNamePolicy
+
+  [@@@ocamlformat "disable"]
+
+  (** TODO: This *)
+  let reserved_words = Hash_set.of_list (module String) [] 
+end
+
+module U = Ast_utils.MakeWithNamePolicy (InputLanguage) (LeanNamePolicy)
+module GenPrint = Generic_printer.Make (InputLanguage) (U.Concrete_ident_view)
+
+let translate (_opts: BackendOptions.t) (items: AST.item list) =
+  U.group_items_by_namespace items
+  |> Map.to_alist
+  |> List.map ~f:(fun (ns, items) ->
+         let mod_name =
+           String.concat ~sep:"."
+             (List.map
+                ~f:(map_first_letter String.uppercase)
+                (fst ns :: snd ns))
+         in
+         Types.
+           {
+             path = mod_name ^ ".lean";
+             contents = GenPrint.items items |> fst;
+           })
