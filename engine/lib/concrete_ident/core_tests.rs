c721a5913d81c74236e0acc7321d6da0bfd99009
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-04 15:58:42 +0200

    wip: add names for testing partial core extract

---
+++ engine/lib/concrete_ident/core_tests.rs
@@ -0,0 +1,18 @@
+#![allow(dead_code)]
+#![feature(try_trait_v2)]
+#![feature(allocator_api)]
+extern crate alloc;
+/* This is a dummy Rust file. Every path used in this file will be
+ * exported and made available automatically in OCaml. */
+
+fn dummy_hax_concrete_ident_wrapper() {
+    fn hello<T: core::ops::Add<T, Output = T>>(x: T) -> T {
+        x + x
+    }
+    let _: Option<u8> = Some(3);
+    let mut acc = vec![];
+    for i in 0..10 {
+        acc.push(i.clone());
+    }
+    let _ = acc.iter().map(|x| x + 1);
+}
