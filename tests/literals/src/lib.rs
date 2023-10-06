1a87f8af925abb08d150605fe239b51ecfbaae60
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-06 10:34:02 +0200

    test: casts

--- tests/literals/src/lib.rs
+++ tests/literals/src/lib.rs
@@ -33,3 +33,14 @@ pub fn patterns() {
         _ => (),
     };
 }
+
+fn casts(x8: u8, x16: u16, x32: u32, x64: u64, xs: usize) {
+    let _: u64 = x8 as u64 + x16 as u64 + x32 as u64 + x64 as u64 + xs as u64;
+    let _: u32 = x8 as u32 + x16 as u32 + x32 as u32 + x64 as u32 + xs as u32;
+    let _: u16 = x8 as u16 + x16 as u16 + x32 as u16 + x64 as u16 + xs as u16;
+    let _: u8 = x8 as u8 + x16 as u8 + x32 as u8 + x64 as u8 + xs as u8;
+    let _: i64 = x8 as i64 + x16 as i64 + x32 as i64 + x64 as i64 + xs as i64;
+    let _: i32 = x8 as i32 + x16 as i32 + x32 as i32 + x64 as i32 + xs as i32;
+    let _: i16 = x8 as i16 + x16 as i16 + x32 as i16 + x64 as i16 + xs as i16;
+    let _: i8 = x8 as i8 + x16 as i8 + x32 as i8 + x64 as i8 + xs as i8;
+}
