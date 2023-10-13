9629c44a1e76039dbb2e9efe747aedddb8c6e5f6
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-10-13 10:46:47 +0200

    feat(test-harness): better output for stdout

--- test-harness/src/harness.rs
+++ test-harness/src/harness.rs
@@ -168,6 +168,16 @@ impl Test {
             snapshot.insert(
                 "stdout".to_string(),
                 serde_json::from_str(&sout)
+                    .map(|out: hax_cli_options_engine::Output| {
+                        use serde_json::json;
+                        json!({
+                            "diagnostics": Value::Array(out.diagnostics.into_iter().map(|diag| json!({
+                                "spans": Value::Array(diag.span.clone().into_iter().map(|span| Value::String(format!("{:?}", span))).collect()),
+                                "message": Value::String(format!("{}", diag)),
+                            })).collect()),
+                            "files": Value::Object(out.files.into_iter().map(|file| (file.path, Value::String(file.contents))).collect())
+                        })
+                    })
                     .unwrap_or_else(|_| Value::String(cleanup(sout.clone()))),
             );
         }
