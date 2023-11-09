31d7903b93ff4964950919c928dc36f708dc4575
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-11-09 08:29:10 +0100

    feat(engine): add `generic_args` on fun. calls

--- engine/lib/dependencies.mli
+++ engine/lib/dependencies.mli
@@ -2,7 +2,7 @@ module Make (F : Features.T) : sig
   module AST : module type of Ast.Make (F)
 
   val name_me : AST.item list -> AST.item list
-  val sort: AST.item list -> AST.item list
+  val sort : AST.item list -> AST.item list
 
   val filter_by_inclusion_clauses :
     Types.inclusion_clause list -> AST.item list -> AST.item list
