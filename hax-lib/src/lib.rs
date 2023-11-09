71b2ae32778f024aed05fe1a82099b0d99204181
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-11-09 15:19:41 +0100

    feat(hax-lib): intro hax-lib
    This commit adds a crate named `hax-lib`.
    
    This crate contains hax-specific helpers for Rust programs. Those
    helpers are usually no-ops when compiled normally but meaningful when
    compiled under hax.
    
    For now, essentially, this adds an assert and an assume macro.

---
+++ hax-lib/src/lib.rs
@@ -0,0 +1,52 @@
+#[cfg(hax_compilation)]
+#[macro_export]
+macro_rules! proxy_macro_if_not_hax {
+    ($macro:path, no, $($arg:tt)*) => {
+        ()
+    };
+    ($macro:path, $f:expr, $cond:expr$(, $($arg:tt)*)?) => {
+        $f($cond)
+    };
+}
+#[cfg(not(hax_compilation))]
+#[macro_export]
+macro_rules! proxy_macro_if_not_hax {
+    ($macro:path, $f:expr, $($arg:tt)*) => {
+        $macro!($($arg)*)
+    };
+}
+
+#[macro_export]
+macro_rules! debug_assert {
+    ($($arg:tt)*) => {
+        $crate::proxy_macro_if_not_hax!(::std::debug_assert, no, $($arg)*)
+    };
+}
+
+#[macro_export]
+macro_rules! assert {
+    ($($arg:tt)*) => {
+        $crate::proxy_macro_if_not_hax!(::std::assert, $crate::assert, $($arg)*)
+    };
+}
+
+#[cfg(hax_compilation)]
+pub fn assert(_formula: bool) {}
+
+#[cfg(hax_compilation)]
+pub fn assume(_formula: bool) {}
+
+#[cfg(hax_compilation)]
+#[macro_export]
+macro_rules! assume {
+    ($formula:expr) => {
+        $crate::assume($formula)
+    };
+}
+#[cfg(not(hax_compilation))]
+#[macro_export]
+macro_rules! assume {
+    ($formula:expr) => {
+        ()
+    };
+}
