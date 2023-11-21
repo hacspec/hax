661ef96d9135064ccdff2c29149dbdd16d72cb81
Lasse Letager Hansen <lasse@letager.dk>
2023-11-21 17:57:43 +0100

    Simplified analysis, swapped from cid to strings

--- engine/lib/analyses/function_dependency.ml
+++ engine/lib/analyses/function_dependency.ml
@@ -6,35 +6,21 @@ module%inlined_contents Make (F : Features.T) = struct
   module U = Ast_utils.Make (F)
   open Ast
 
-  type analysis_data = concrete_ident list Map.M(Concrete_ident).t
+  type analysis_data = concrete_ident list Map.M(String).t (* Concrete_ident *)
   type id_order = int
 
-  let analyse_function_body (x : A.expr) : concrete_ident list =
-    Set.to_list (U.Reducers.collect_concrete_idents#visit_expr () x)
-  (* List.filter_map *)
-  (*   ~f:(function *)
-  (*     | `Projector (`Concrete cid) | `Concrete cid -> *)
-  (*         Some (Uprint.Concrete_ident_view.to_definition_name cid) *)
-  (*     | _ -> None) *)
-  (*   (Set.to_list collect_global_idents) *)
-
-  let functions_of_item (x : A.item) : (concrete_ident * A.expr) list =
-    match x.v with
-    | Fn { name; generics = _; body; params = _ } -> [ (name, body) ]
-    | Impl { items; _ } ->
-        List.filter_map
-          ~f:(fun w ->
-            match w.ii_v with
-            | IIFn { body; params = _ } -> Some (w.ii_ident, body)
-            | _ -> None)
-          items
-    | _ -> []
+  module Uprint =
+    Ast_utils.MakeWithNamePolicy (F) (Concrete_ident.DefaultNamePolicy)
 
   let analyse (items : A.item list) : analysis_data =
-    let temp_list = List.concat_map ~f:functions_of_item items in
+    let temp_list = List.concat_map ~f:U.functions_of_item items in
     List.fold_left
-      ~init:(Map.empty (module Concrete_ident))
+      ~init:(Map.empty (module String (* Concrete_ident *)))
       ~f:(fun y (name, body) ->
-        Map.set y ~key:name ~data:(analyse_function_body body))
+        Map.set y
+          ~key:(Uprint.Concrete_ident_view.to_definition_name name)
+          ~data:
+            (Set.to_list
+               (U.Reducers.collect_concrete_idents#visit_expr () body)))
       temp_list
 end
