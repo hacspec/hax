6990633c8ae5435056f0ad460eecd4995658707a
Lucas Franceschino <lucas.franceschino@inria.fr>
2023-11-21 09:57:31 +0100

    feat: add `Self` impl source

--- tests/traits/src/lib.rs
+++ tests/traits/src/lib.rs
@@ -1,12 +1,24 @@
 #![allow(dead_code)]
 
+pub trait SuperTrait: Clone {
+    fn function_of_super_trait(self) -> u32;
+}
+
 pub trait Foo {
+    type AssocType: SuperTrait;
     const N: usize;
     fn assoc_f() -> ();
     fn method_f(&self) -> ();
 }
 
+impl SuperTrait for i32 {
+    fn function_of_super_trait(self) -> u32 {
+        self.abs() as u32
+    }
+}
+
 impl Foo for () {
+    type AssocType = i32;
     const N: usize = 32;
     fn assoc_f() {
         ()
@@ -19,4 +31,8 @@ impl Foo for () {
 fn f<T: Foo>(x: T) {
     T::assoc_f();
     x.method_f()
+}
+
+fn g<T: Foo>(x: T::AssocType) -> u32 {
+    x.function_of_super_trait()
 }
