a1491dddb09a6cfb7c03cc0f4c3d340f8f244799
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-01-25 14:09:32 +0100

    Choose correct algorithms in noise example

--- tests/proverif-noise/src/noise_crypto.rs
+++ tests/proverif-noise/src/noise_crypto.rs
@@ -1,5 +1,5 @@
 // Import hacspec and all needed definitions.
-use hax_lib_protocol::cal::*;
+use hax_lib_protocol::cal::{DHGroup, *};
 
 /// This file formalizes the Crypto Functions from the Noise Specification
 /// Section 4: Crypto Functions
@@ -19,7 +19,7 @@ pub const DHLEN: usize = 32;
 
 pub fn generate_keypair(sk: &[u8]) -> KeyPair {
     let sk = DHScalar::from_bytes(sk);
-    let pk = dh_scalar_multiply_base(sk.clone());
+    let pk = dh_scalar_multiply_base(DHGroup::X25519, sk.clone());
     KeyPair {
         private_key: sk,
         public_key: pk,
@@ -28,8 +28,8 @@ pub fn generate_keypair(sk: &[u8]) -> KeyPair {
 
 pub fn dh(sk: &KeyPair, pk: &[u8]) -> Vec<u8> {
     let pk = DHElement::from_bytes(pk);
-    
-    dh_scalar_multiply(sk.private_key.clone(), pk)
+
+    dh_scalar_multiply(DHGroup::X25519, sk.private_key.clone(), pk)
 }
 
 /// Section 4.2 and 12.3: Cipher functions for ChaCha20-Poly1305
@@ -38,7 +38,7 @@ pub fn encrypt(key: &[u8], counter: u64, aad: &[u8], plain: &[u8]) -> Vec<u8> {
     let mut chacha_iv = vec![0u8; 4];
     chacha_iv.extend_from_slice(&counter.to_le_bytes());
     let (mut cipher, tag) = aead_encrypt(
-        AEADKey::from_bytes(key),
+        AEADKey::from_bytes(AEADAlgorithm::Chacha20Poly1305, key),
         AEADIV::from_bytes(&chacha_iv),
         aad,
         plain,
@@ -54,7 +54,7 @@ pub fn decrypt(key: &[u8], counter: u64, aad: &[u8], cipher: &[u8]) -> Result<Ve
     let cip = &cipher[0..cipher_len];
     let tag = &cipher[cipher_len..cipher.len()];
     aead_decrypt(
-        AEADKey::from_bytes(key),
+        AEADKey::from_bytes(AEADAlgorithm::Chacha20Poly1305, key),
         AEADIV::from_bytes(&chacha_iv),
         aad,
         cip,
@@ -73,11 +73,11 @@ pub const HASHLEN: usize = 32;
 pub const BLOCKLEN: usize = 64;
 
 pub fn hash(input: &[u8]) -> Vec<u8> {
-    hax_lib_protocol::cal::hash(input)
+    hax_lib_protocol::cal::hash(HashAlgorithm::Sha256, input)
 }
 
 pub fn hmac_hash(key: &[u8], input: &[u8]) -> Vec<u8> {
-    hmac(key, input)
+    hmac(HMACAlgorithm::Sha256, key, input)
 }
 
 /// HKDF spec as per Noise
