d3d2676bb5188019a80dec6e2beb1e717871db4b
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-03-07 17:23:20 +0100

    update docker

--- .docker/install.sh
+++ .docker/install.sh
@@ -17,10 +17,13 @@ cd $HOME/hax
 cd ~/
 
 # Get F* and HACL* for running proofs
-curl -L https://github.com/FStarLang/FStar/releases/download/v2024.01.13/fstar_2024.01.13_Linux_x86_64.tar.gz \
-    --output Fstar.tar.gz
-tar --extract --file Fstar.tar.gz
-rm -f Fstar.tar.gz
+# curl -L https://github.com/FStarLang/FStar/releases/download/v2024.01.13/fstar_2024.01.13_Linux_x86_64.tar.gz \
+#     --output Fstar.tar.gz
+# tar --extract --file Fstar.tar.gz
+curl -L https://github.com/FStarLang/FStar/archive/refs/tags/v2024.01.13.zip --output Fstar.zip
+unzip Fstar.zip
+rm -f Fstar.zip
+mv FStar-2024.01.13 fstar
 curl -L https://github.com/hacl-star/hacl-star/archive/refs/heads/main.zip \
     --output hacl-star.zip
 unzip hacl-star.zip
@@ -30,4 +33,5 @@ echo "export FSTAR_HOME=~/fstar" >>$HOME/.bashrc
 echo "export HACL_HOME=~/hacl-star" >>$HOME/.bashrc
 echo "export HAX_HOME=~/hax" >>$HOME/.bashrc
 echo "export PATH=\"${PATH}:~/fstar/bin\"" >>$HOME/.bashrc
+echo "alias fstar.exe='fstar.exe --z3version 4.8.12'" >>$HOME/.bashrc
 echo "[[ ! -r /home/$USER/.opam/opam-init/init.sh ]] || source /home/$USER/.opam/opam-init/init.sh  > /dev/null 2> /dev/null" >>$HOME/.bashrc
