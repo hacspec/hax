f584861a3acc70307d03d90affb34ef6995a1e4f
Lucas Franceschino <lucas@cryspen.com>
2024-03-07 15:38:18 +0100

    feat(engine/phases): intro `drop_needless_returns`

--- engine/lib/phases.ml
+++ engine/lib/phases.ml
@@ -11,4 +11,5 @@ module Functionalize_loops = Phase_functionalize_loops.Make
 module Reject = Phase_reject
 module Local_mutation = Phase_local_mutation.Make
 module Traits_specs = Phase_traits_specs.Make
+module Drop_needless_returns = Phase_drop_needless_returns.Make
 module Drop_sized_trait = Phase_drop_sized_trait.Make
