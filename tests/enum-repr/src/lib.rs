d33b30a2c2b2dd1daa8b6f66ae3d2e631c964791
Lucas Franceschino <lucas@cryspen.com>
2024-03-13 15:46:05 +0100

    feat: translate enum descriminant and their casts

--- tests/enum-repr/src/lib.rs
+++ tests/enum-repr/src/lib.rs
@@ -4,11 +4,20 @@
 enum Foo {
     A = 1,
     B = 5,
-    C = 9,
+    C(),
+    D {},
 }
 
 fn f() -> u16 {
     const CONST: u16 = Foo::A as u16;
     let _x = Foo::B as u16;
     Foo::C as u16
+}
+
+fn get_repr(x: Foo) -> u16 {
+    x as u16
+}
+
+fn get_casted_repr(x: Foo) -> u64 {
+    x as u64
 }
