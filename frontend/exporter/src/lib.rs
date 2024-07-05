0d59b0b2435244ab338213810f9761c92bfd60dc
Lucas Franceschino <lucas@cryspen.dev>
2024-07-05 16:28:19 +0200

    remove `cfg_if`

--- frontend/exporter/src/lib.rs
+++ frontend/exporter/src/lib.rs
@@ -9,36 +9,43 @@
 #![allow(rustdoc::private_intra_doc_links)]
 #![cfg_attr(feature = "rustc", feature(rustc_private))]
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "rustc")] {
-        // When the feature `rustc` is enabled, we enable the bridges
-        // between rustc ASTs, which are defined in the crates
-        // `rustc_*`. We thus need to import them with `extern crate
-        // rustc_*`
-        extern crate rustc_abi;
-        extern crate rustc_ast;
-        extern crate rustc_ast_pretty;
-        extern crate rustc_data_structures;
-        extern crate rustc_driver;
-        extern crate rustc_errors;
-        extern crate rustc_hir;
-        extern crate rustc_hir_analysis;
-        extern crate rustc_index;
-        extern crate rustc_infer;
-        extern crate rustc_interface;
-        extern crate rustc_middle;
-        extern crate rustc_mir_build;
-        extern crate rustc_session;
-        extern crate rustc_span;
-        extern crate rustc_target;
-        extern crate rustc_trait_selection;
-        extern crate rustc_type_ir;
-
-        mod rustc_utils;
-        pub mod state;
-        mod utils;
-        mod deterministic_hash;
+macro_rules! cfg_feature_rustc {
+    ($($item:item)*) => {
+        $(
+            #[cfg(feature = "rustc")]
+            $item
+        )*
     }
+}
+
+cfg_feature_rustc! {
+    // When the feature `rustc` is enabled, we enable the bridges
+    // between rustc ASTs, which are defined in the crates
+    // `rustc_*`. We thus need to import them with `extern crate
+    // rustc_*`
+    extern crate rustc_abi;
+    extern crate rustc_ast;
+    extern crate rustc_ast_pretty;
+    extern crate rustc_data_structures;
+    extern crate rustc_driver;
+    extern crate rustc_errors;
+    extern crate rustc_hir;
+    extern crate rustc_hir_analysis;
+    extern crate rustc_index;
+    extern crate rustc_infer;
+    extern crate rustc_interface;
+    extern crate rustc_middle;
+    extern crate rustc_mir_build;
+    extern crate rustc_session;
+    extern crate rustc_span;
+    extern crate rustc_target;
+    extern crate rustc_trait_selection;
+    extern crate rustc_type_ir;
+
+    mod rustc_utils;
+    pub mod state;
+    mod utils;
+    mod deterministic_hash;
 }
 
 mod body;
