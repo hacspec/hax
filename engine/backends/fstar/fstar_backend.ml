c5c22ca5606d9a75f36365e7a0f94b9302958d49
Maxime Buyse <maxime@cryspen.com>
2024-07-22 16:13:32 +0200

    Resugar asserts.

--- engine/backends/fstar/fstar_backend.ml
+++ engine/backends/fstar/fstar_backend.ml
@@ -1622,6 +1622,7 @@ module TransformToInputLanguage =
   |> Phases.Drop_sized_trait
   |> Phases.Simplify_question_marks
   |> Phases.And_mut_defsite
+  |> Phases.Reconstruct_asserts
   |> Phases.Reconstruct_for_loops
   |> Phases.Reconstruct_while_loops
   |> Phases.Direct_and_mut
