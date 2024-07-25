9b756ab7de3e8dc4877038bc90363c0d6abc976b
Lucas Franceschino <lucas@cryspen.com>
2024-07-25 09:27:54 +0200

    feat(diagnostics): `HaxMessage`: add `code()` function

--- hax-types/src/diagnostics/message.rs
+++ hax-types/src/diagnostics/message.rs
@@ -3,22 +3,37 @@ use crate::prelude::*;
 
 #[derive_group(Serializers)]
 #[derive(Debug, Clone, JsonSchema)]
+#[repr(u8)]
 pub enum HaxMessage {
     Diagnostic {
         diagnostic: super::Diagnostics,
         working_dir: PathBuf,
-    },
+    } = 254,
     EngineNotFound {
         is_opam_setup_correctly: bool,
-    },
+    } = 0,
     WroteFile {
         path: PathBuf,
-    },
+    } = 1,
     HaxEngineFailure {
         exit_code: i32,
-    },
-    CargoBuildFailure,
+    } = 2,
+    CargoBuildFailure = 3,
     WarnExperimentalBackend {
         backend: Backend,
-    },
+    } = 4,
+}
+
+impl HaxMessage {
+    // https://doc.rust-lang.org/reference/items/enumerations.html#pointer-casting
+    pub fn discriminant(&self) -> u16 {
+        unsafe { *(self as *const Self as *const u16) }
+    }
+
+    pub fn code(&self) -> String {
+        match self {
+            HaxMessage::Diagnostic { diagnostic, .. } => diagnostic.kind.code(),
+            _ => format!("CARGOHAX{:0>4}", self.discriminant()),
+        }
+    }
 }
