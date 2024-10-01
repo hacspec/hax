f2783c0eb3183dbb7b1e13f43887bd34fcc6d5f7
Maxime Buyse <maxime@cryspen.com>
2024-10-01 14:10:06 +0200

    Implement bundling for cyclic module dependencies.

--- engine/backends/fstar/fstar_backend.ml
+++ engine/backends/fstar/fstar_backend.ml
@@ -96,6 +96,11 @@ module Context = struct
   }
 end
 
+(** Convers a namespace to a module name *)
+let module_name (ns : string * string list) : string =
+  String.concat ~sep:"."
+    (List.map ~f:(map_first_letter String.uppercase) (fst ns :: snd ns))
+
 module Make
     (Attrs : Attrs.WITH_ITEMS) (Ctx : sig
       val ctx : Context.t
@@ -856,19 +861,24 @@ struct
       list =
     match e.v with
     | Alias { name; item } ->
-        let pat =
-          F.pat
-          @@ F.AST.PatVar
-               (F.id @@ U.Concrete_ident_view.to_definition_name name, None, [])
-        in
-        F.decls ~quals:[ F.AST.Unfold_for_unification_and_vcgen ]
-        @@ F.AST.TopLevelLet
-             ( NoLetQualifier,
-               [ (pat, F.term @@ F.AST.Name (pconcrete_ident item)) ] )
+        (* let pat =
+             F.pat
+             @@ F.AST.PatVar
+                  (F.id @@ U.Concrete_ident_view.to_definition_name name, None, [])
+           in
+           F.decls ~quals:[ F.AST.Unfold_for_unification_and_vcgen ]
+           @@ F.AST.TopLevelLet
+                ( NoLetQualifier,
+                  [ (pat, F.term @@ F.AST.Name (pconcrete_ident item)) ] ) *)
+        let bundle = U.Concrete_ident_view.to_namespace item |> module_name in
+        [
+          `VerbatimImpl
+            ( Printf.sprintf "include %s {%s as %s}" bundle
+                (U.Concrete_ident_view.to_definition_name item)
+                (U.Concrete_ident_view.to_definition_name name),
+              `Newline );
+        ]
     | Fn { name; generics; body; params } ->
-        let is_rec =
-          Set.mem (U.Reducers.collect_concrete_idents#visit_expr () body) name
-        in
         let name = F.id @@ U.Concrete_ident_view.to_definition_name name in
         let pat = F.pat @@ F.AST.PatVar (name, None, []) in
         let generics = FStarBinder.of_generics e.span generics in
@@ -881,7 +891,7 @@ struct
               params
         in
         let pat = F.pat @@ F.AST.PatApp (pat, pat_args) in
-        let qualifier = F.AST.(if is_rec then Rec else NoLetQualifier) in
+        let qualifier = F.AST.(NoLetQualifier) in
         let impl =
           F.decl ~fsti:false
           @@ F.AST.TopLevelLet (qualifier, [ (pat, pexpr body) ])
@@ -916,7 +926,11 @@ struct
         in
         let pat = F.pat @@ F.AST.PatAscribed (pat, (ty, None)) in
         let full =
-          F.decl @@ F.AST.TopLevelLet (qualifier, [ (pat, pexpr body) ])
+          F.decl
+          @@ F.AST.TopLevelLet
+               ( qualifier,
+                 [ (pat, pexpr body) ]
+                 (* TODO there should be several elements for mut rec*) )
         in
 
         let intf = F.decl ~fsti:true (F.AST.Val (name, arrow_typ)) in
@@ -1553,10 +1567,9 @@ let strings_of_item ~signature_only (bo : BackendOptions.t) m items
            if interface_mode then [ (`Impl s, `Newline); (`Intf s, `Newline) ]
            else [ (`Impl s, `Newline) ])
 
-(** Convers a namespace to a module name *)
-let module_name (ns : string * string list) : string =
-  String.concat ~sep:"."
-    (List.map ~f:(map_first_letter String.uppercase) (fst ns :: snd ns))
+module DepGraph = Dependencies.Make (InputLanguage)
+
+type rec_prefix = NonRec | FirstMutRec | MutRec
 
 let string_of_items ~signature_only ~mod_name (bo : BackendOptions.t) m items :
     string * string =
@@ -1599,14 +1612,48 @@ let string_of_items ~signature_only ~mod_name (bo : BackendOptions.t) m items :
           |> String.concat ~sep:"")
         ^ "\n\n"
   in
-  let strings =
-    List.concat_map ~f:(strings_of_item ~signature_only bo m items) items
-    |> List.map
-         ~f:
-           ((function
-              | `Impl s -> `Impl (String.strip s)
-              | `Intf s -> `Intf (String.strip s))
-           *** Fn.id)
+  let map_string ~f (str, space) =
+    ((match str with `Impl s -> `Impl (f s) | `Intf s -> `Intf (f s)), space)
+  in
+  let replace_in_strs ~pattern ~with_ =
+    List.map
+      ~f:
+        (map_string ~f:(fun str ->
+             Logs.info (fun m -> m "%s \n" str);
+             String.substr_replace_first ~pattern ~with_ str))
+  in
+  let bundles, _ = DepGraph.recursive_bundles items in
+  let first_in_bundles = Array.create (List.length bundles) None in
+  let get_recursivity_prefix it =
+    match
+      List.findi bundles ~f:(fun _ bundle ->
+          List.mem bundle it ~equal:[%eq: item])
+    with
+    | Some (i, _) -> (
+        match first_in_bundles.(i) with
+        | Some first_it when [%eq: item] first_it it -> FirstMutRec
+        | Some _ -> MutRec
+        | None ->
+            first_in_bundles.(i) <- Some it;
+            FirstMutRec)
+    | None -> NonRec
+  in
+  let strings its =
+    List.concat_map
+      ~f:(fun item ->
+        let recursivity_prefix = get_recursivity_prefix item in
+        let strs = strings_of_item ~signature_only bo m items item in
+        match (recursivity_prefix, item.v) with
+        | FirstMutRec, Fn _ ->
+            Logs.info (fun m -> m "replace let rec in \n\n");
+            replace_in_strs ~pattern:"let" ~with_:"let rec" strs
+        | MutRec, Fn _ ->
+            Logs.info (fun m -> m "replace and in \n\n");
+            replace_in_strs ~pattern:"let" ~with_:"and" strs
+        | MutRec, Type _ -> replace_in_strs ~pattern:"type" ~with_:"and" strs
+        | _ -> strs)
+      its
+    |> List.map ~f:(map_string ~f:String.strip)
     |> List.filter
          ~f:(fst >> (function `Impl s | `Intf s -> String.is_empty s) >> not)
   in
@@ -1616,7 +1663,7 @@ let string_of_items ~signature_only ~mod_name (bo : BackendOptions.t) m items :
         ~f:(fun (s, space) ->
           let* s = filter s in
           Some (s, space))
-        strings
+        (strings items)
     in
     let n = List.length l - 1 in
     let lines =
@@ -1680,7 +1727,6 @@ let translate m (bo : BackendOptions.t) (items : AST.item list) :
            [ make ~ext:"fst" impl; make ~ext:"fsti" intf ])
 
 open Phase_utils
-module DepGraph = Dependencies.Make (InputLanguage)
 module DepGraphR = Dependencies.Make (Features.Rust)
 
 module TransformToInputLanguage =
@@ -1760,11 +1806,12 @@ let apply_phases (bo : BackendOptions.t) (items : Ast.Rust.item list) :
     (* else *)
     items
   in
+  Logs.info (fun m -> m "bundles: here\n\n");
   let items =
     TransformToInputLanguage.ditems items
     |> List.map ~f:unsize_as_identity
     |> List.map ~f:unsize_as_identity
     |> List.map ~f:U.Mappers.add_typ_ascription
-    (* |> DepGraph.name_me *)
+    |> DepGraph.name_me
   in
   items
--- engine/lib/dependencies.mli
+++ engine/lib/dependencies.mli
@@ -4,6 +4,7 @@ module Make (F : Features.T) : sig
   val uid_associated_items : AST.item list -> Ast.attrs -> AST.item list
   val name_me : AST.item list -> AST.item list
   val sort : AST.item list -> AST.item list
+  val recursive_bundles : AST.item list -> AST.item list list * AST.item list
 
   val filter_by_inclusion_clauses :
     Types.inclusion_clause list -> AST.item list -> AST.item list
