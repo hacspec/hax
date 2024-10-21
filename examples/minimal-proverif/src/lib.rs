5d72ac69112c0738cf55a102b2d85c3f3eecff40
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-21 16:09:34 +0200

    Formatting

--- examples/minimal-proverif/src/lib.rs
+++ examples/minimal-proverif/src/lib.rs
@@ -3,7 +3,6 @@ pub enum Message {
     Pong(u8),
 }
 
-
 pub fn send_ping(input: u8) -> Message {
     Message::Ping(input)
 }
@@ -11,14 +10,13 @@ pub fn send_ping(input: u8) -> Message {
 pub fn receive_ping(message: Message) -> Result<Message, ()> {
     match message {
         Message::Ping(payload) => Ok(Message::Pong(payload)),
-        _ => Err(())
+        _ => Err(()),
     }
 }
 
 pub fn receive_pong(message: Message) -> Result<u8, ()> {
     match message {
         Message::Pong(payload) => Ok(payload),
-        _ => Err(())
+        _ => Err(()),
     }
 }
-
