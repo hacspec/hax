ef6607f9048e2fdf03b5d1e6080e95959def45b7
Lucas Franceschino <lucas@cryspen.com>
2024-11-13 14:03:31 +0100

    fix: revert cfg: we don't want to filter things out

--- hax-lib/src/proc_macros.rs
+++ hax-lib/src/proc_macros.rs
@@ -2,11 +2,9 @@
 //! proc-macro crate cannot export anything but procedural macros.
 
 pub use hax_lib_macros::{
-    attributes, ensures, exclude, include, lemma, opaque_type, refinement_type, requires,
+    attributes, ensures, exclude, impl_fn_decoration, include, lemma, loop_invariant, opaque_type,
+    refinement_type, requires, trait_fn_decoration,
 };
-
-#[cfg(hax)]
-pub use hax_lib_macros::{impl_fn_decoration, loop_invariant, trait_fn_decoration};
 
 pub use hax_lib_macros::{
     process_init, process_read, process_write, protocol_messages, pv_constructor, pv_handwritten,
