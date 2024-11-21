07851ee8795e4acd52a3eae267d777f1942236b1
Lucas Franceschino <lucas@cryspen.com>
2024-11-21 06:54:47 +0100

    feat(engine/sourcemaps): derive more yojson

--- engine/utils/sourcemaps/mappings/types.ml
+++ engine/utils/sourcemaps/mappings/types.ml
@@ -1,4 +1,6 @@
 open Prelude
 
-type meta = { file_offset : int; name : int option } [@@deriving show, eq]
-type point = Location.t Dual.t * meta option [@@deriving show, eq]
+type meta = { file_offset : int; name : int option }
+[@@deriving show, eq, yojson]
+
+type point = Location.t Dual.t * meta option [@@deriving show, eq, yojson]
