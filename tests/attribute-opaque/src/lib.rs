c5dc7188c6ff1b862cf906d6899ede79df73e677
Maxime Buyse <maxime@cryspen.com>
2024-12-05 13:06:01 +0100

    Updated tests.

--- tests/attribute-opaque/src/lib.rs
+++ tests/attribute-opaque/src/lib.rs
@@ -11,9 +11,22 @@ enum OpaqueEnum<const X: usize, T, U> {
 }
 
 #[hax_lib::opaque]
+fn f_generic<const X: usize, T, U>(x: U) -> OpaqueEnum<X, T, U> {
+    OpaqueEnum::B(x)
+}
+
+#[hax_lib::opaque]
 fn f(x: bool, y: bool) -> bool {
     x && y
 }
+
+#[hax_lib::opaque]
+#[hax_lib::requires(x)]
+#[hax_lib::ensures(|result| result == y)]
+fn f_pre_post(x: bool, y: bool) -> bool {
+    x && y
+}
+
 trait T {
     fn d();
 }
@@ -23,12 +36,23 @@ impl T for u8 {
     fn d() {
         unsafe {
             let my_num: i32 = 10;
-            let my_num_ptr: *const i32 = &my_num;
+            let _my_num_ptr: *const i32 = &my_num;
             let mut my_speed: i32 = 88;
-            let my_speed_ptr: *mut i32 = &mut my_speed;
+            let _my_speed_ptr: *mut i32 = &mut my_speed;
         }
     }
 }
 
+trait TrGeneric<U: Clone> {
+    fn f(x: U) -> Self;
+}
+
 #[hax_lib::opaque]
-const c: u8 = 0 + 0;
+impl<U: Clone> TrGeneric<U> for i32 {
+    fn f(_x: U) -> Self {
+        0
+    }
+}
+
+#[hax_lib::opaque]
+const C: u8 = 0 + 0;
