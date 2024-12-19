b7a9e3404fa0132244ac12417ba58b046b5a1817
Maxime Buyse <maxime@cryspen.com>
2024-12-19 14:25:40 +0100

    Add real disambiguator instead of modifying string to resolve name clashes in bundles.

--- engine/lib/dependencies.ml
+++ engine/lib/dependencies.ml
@@ -435,10 +435,7 @@ module Make (F : Features.T) = struct
             (List.mem duplicates (new_name_under_ns id)
                ~equal:Concrete_ident.equal)
         then id
-        else
-          Concrete_ident.Create.map_last
-            ~f:(fun name -> name ^ (Concrete_ident.hash id |> Int.to_string))
-            id
+        else Concrete_ident.Create.add_disambiguator id (Concrete_ident.hash id)
       in
       let renamings =
         List.map
