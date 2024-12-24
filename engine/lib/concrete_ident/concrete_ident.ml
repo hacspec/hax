254990dcb2d80d48c184246681aeb2ceccd867d6
Maxime Buyse <maxime@cryspen.com>
2024-12-24 12:25:50 +0100

    Rename rec_bundle as cyclic_bundle.

--- engine/lib/concrete_ident/concrete_ident.ml
+++ engine/lib/concrete_ident/concrete_ident.ml
@@ -605,7 +605,7 @@ module Create = struct
             id.def_id.path
             @ [
                 {
-                  data = TypeNs "rec_bundle";
+                  data = TypeNs "cyclic_bundle";
                   disambiguator = [%hash: t list] from;
                 };
               ];
@@ -637,11 +637,14 @@ module Create = struct
 
   let add_disambiguator name disambiguator =
     let path = name.def_id.path in
-    let last = List.last_exn path in
-    let path =
-      List.drop_last_exn path @ [ { data = last.data; disambiguator } ]
-    in
-    { name with def_id = { name.def_id with path } }
+    if List.is_empty path then name
+    else
+      (* The following two `exn` function calls cannot fail as the path is not empty. *)
+      let last = List.last_exn path in
+      let path =
+        List.drop_last_exn path @ [ { data = last.data; disambiguator } ]
+      in
+      { name with def_id = { name.def_id with path } }
 end
 
 let lookup_raw_impl_info (impl : t) : Types.impl_infos option =
