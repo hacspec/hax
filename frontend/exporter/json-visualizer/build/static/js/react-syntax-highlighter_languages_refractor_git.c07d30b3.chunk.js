f3a4af425775009a0446f9df699a7dce32640943
Lucas Franceschino <lucas@cryspen.com>
2025-01-20 14:10:39 +0100

    Delete frontend/exporter/json-visualizer directory
    This PR drops `json-visualizer` altogether.
    
    It used to be a web app for inspecting the exporter JSON output, but piping the JSON into `jless` is suffisient today.
    That tool was useful when we had to design the frontend at the begining.
     
    This is stale and no longer useful, thus we drop it.

--- frontend/exporter/json-visualizer/build/static/js/react-syntax-highlighter_languages_refractor_git.c07d30b3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkvisualizer=self.webpackChunkvisualizer||[]).push([[2564],{787:function(e){function i(e){e.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}e.exports=i,i.displayName="git",i.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.c07d30b3.chunk.js.map
