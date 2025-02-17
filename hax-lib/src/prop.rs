edae02e96f05f826bc1595d56721ab9eb0b6cc5d
Lucas Franceschino <lucas@cryspen.com>
2025-02-17 16:51:00 +0100

    feat(hax-lib/prop): define everything in terms of `hax_lib::prop::constructors`

--- hax-lib/src/prop.rs
+++ hax-lib/src/prop.rs
@@ -1,10 +1,72 @@
 use crate::abstraction::*;
 use core::ops::*;
 
+/// Represent a logical proposition, that may be not computable.
+#[derive(Clone, Copy, Debug)]
 pub struct Prop(bool);
 
-pub trait ToProp {
-    fn to_prop(self) -> Prop;
+/// This module provides monomorphic constructors for `Prop`.
+/// Hax rewrite more elaborated versions (see `forall` or `AndBit` below) to those monomorphic constructors.
+pub mod constructors {
+    use super::Prop;
+    pub fn from_bool(b: bool) -> Prop {
+        Prop(b)
+    }
+    pub fn and(lhs: Prop, other: Prop) -> Prop {
+        Prop(lhs.0 && other.0)
+    }
+    pub fn or(lhs: Prop, other: Prop) -> Prop {
+        Prop(lhs.0 || other.0)
+    }
+    pub fn not(lhs: Prop) -> Prop {
+        Prop(!lhs.0)
+    }
+    pub fn eq(lhs: Prop, other: Prop) -> Prop {
+        Prop(lhs.0 == other.0)
+    }
+    pub fn ne(lhs: Prop, other: Prop) -> Prop {
+        Prop(lhs.0 != other.0)
+    }
+    pub fn implies(lhs: Prop, other: Prop) -> Prop {
+        Prop(lhs.0 || !other.0)
+    }
+    pub fn forall<A, F: Fn(A) -> Prop>(_pred: F) -> Prop {
+        Prop(true)
+    }
+    pub fn exists<A, F: Fn(A) -> Prop>(_pred: F) -> Prop {
+        Prop(true)
+    }
+}
+
+impl Prop {
+    /// Lifts a boolean to a logical proposition.
+    pub fn from_bool(b: bool) -> Self {
+        constructors::from_bool(b)
+    }
+    /// Conjuction of two propositions.
+    pub fn and(self, other: impl Into<Self>) -> Self {
+        constructors::and(self, other.into())
+    }
+    /// Disjunction of two propositions.
+    pub fn or(self, other: impl Into<Self>) -> Self {
+        constructors::or(self, other.into())
+    }
+    /// Negation of a proposition.
+    pub fn not(self) -> Self {
+        constructors::not(self)
+    }
+    /// Equality between two propositions.
+    pub fn eq(self, other: impl Into<Self>) -> Self {
+        constructors::eq(self, other.into())
+    }
+    /// Equality between two propositions.
+    pub fn ne(self, other: impl Into<Self>) -> Self {
+        constructors::ne(self, other.into())
+    }
+    /// Logical implication.
+    pub fn implies(self, other: impl Into<Self>) -> Self {
+        constructors::implies(self, other.into())
+    }
 }
 
 impl Abstraction for bool {
@@ -14,6 +76,9 @@ impl Abstraction for bool {
     }
 }
 
+pub trait ToProp {
+    fn to_prop(self) -> Prop;
+}
 impl ToProp for bool {
     fn to_prop(self) -> Prop {
         self.lift()
@@ -53,8 +118,8 @@ impl Not for Prop {
 /// # Example:
 ///
 /// The Rust expression `forall(|x: T| phi(x))` corresponds to `∀ (x: T), phi(x)`.
-pub fn forall<T,U:Into<Prop>>(_f: impl Fn(T) -> U) -> Prop {
-    Prop(true)
+pub fn forall<T, U: Into<Prop>>(f: impl Fn(T) -> U) -> Prop {
+    constructors::forall(|x| f(x).into())
 }
 
 /// The existential quantifier. This should be used only for Hax code: in
@@ -63,11 +128,11 @@ pub fn forall<T,U:Into<Prop>>(_f: impl Fn(T) -> U) -> Prop {
 /// # Example:
 ///
 /// The Rust expression `exists(|x: T| phi(x))` corresponds to `∃ (x: T), phi(x)`.
-pub fn exists<T,U:Into<Prop>>(_f: impl Fn(T) -> U) -> Prop {
-    Prop(true)
+pub fn exists<T, U: Into<Prop>>(f: impl Fn(T) -> U) -> Prop {
+    constructors::exists(|x| f(x).into())
 }
 
 /// The logical implication `a ==> b`.
-pub fn implies<T:Into<Prop>, U:Into<Prop>>(lhs: T, rhs: impl Fn() -> U) -> Prop {
-    !lhs.into() | rhs().into()
+pub fn implies(lhs: impl Into<Prop>, rhs: impl Into<Prop>) -> Prop {
+    constructors::implies(lhs.into(), rhs.into())
 }
